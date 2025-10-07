599a158e0f151c036bba490d6eee55a5f8098cac
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-07 15:34:38 +0530

    adding

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-
 // Factorial
 
-function factorial(){
-    let cache={}
+function factorial() {
+  let cache = {};
 
-    return function inner(n){
+  return function inner(n) {
+    if (n <= 1) return 1;
 
-        if(n<=1) return 1
+    if (cache[n]) {
+      console.log("fetching from cache :");
 
-        if(cache[n]) {
-            console.log("fetching from cache :")
-
-            return n
-        }
-
-
-
+      return cache[n];
     }
 
+    console.log("calculating: ");
+
+    cache[n] = n * inner(n - 1);
+
+    return cache[n];
+  };
+  
 }
 
+const display = factorial();
 
-const display=factorial()
-
-console.log(display())
+console.log(display(5));
+console.log(display(5));
+console.log(display(6));
